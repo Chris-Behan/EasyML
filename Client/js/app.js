560848acaa9257e96ed4645a68fe49de65c27e5f
Anton Lysov <anton.lysov@gmail.com>
2019-01-19 15:33:40 -0700

    Add client code

---
+++ Client/js/app.js
@@ -0,0 +1,18 @@
+// Welcome view asking to provide an CSV file
+function renderWelcomeView() {
+    //$( ".sup" ).text("Next Step...");
+    console.log('sup from Welcome View');
+}
+
+// Pretraining view asking to provide predictions and an attribute
+function renderPretrainingView() {
+    console.log('sup from Pretraining View');
+}
+
+// Result view that provides the accuracy of the trained model as well as
+// allows to predict an attribute based on predictors
+function renderResultView() {
+
+}
+
+renderWelcomeView();
